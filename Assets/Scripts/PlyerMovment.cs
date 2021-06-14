e781e26b363e1da0941c811f0be58329346f57a6
ahmed alnour <ahmed2699@gmail.com>
2021-06-14 12:47:13 +0200

    new change

--- Assets/Scripts/PlyerMovment.cs
+++ Assets/Scripts/PlyerMovment.cs
@@ -2,18 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlyerMovment : MonoBehaviour
-{
+public class PlyerMovment : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         string Name = "plyer";
-        int age = 20;
+        int age = 19;
     }
 }
